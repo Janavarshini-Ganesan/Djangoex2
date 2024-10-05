f6520e1b2ef16d854cc6f66cc49c2cb0d1524aab
Janavarshini-Ganesan <163017120+Janavarshini-Ganesan@users.noreply.github.com>
2024-10-05 06:08:07 +0530

    Add files via upload

---
+++ myapp/views.py
@@ -0,0 +1,26 @@
+from django.shortcuts import render
+
+# Create your views here.
+def home(request):
+    number_result = None
+    name_result = None
+
+    if request.method == 'GET':
+        number = request.GET.get('number')
+        if number:
+            number = int(number)
+            if number % 2 == 0:
+                number_result = f"{number} is an even number"
+            else:
+                number_result = f"{number} is an odd number"
+       
+        char = request.GET.get('character')
+        if char:
+            names = ['Hello', 'Hi', 'Harry', 'Hexa', 'Henry', 'Jack', 'Johny', 'Jeya', 'Varshini', 'Varsha']
+            name_result = [name for name in names if name.startswith(char)]
+
+    context = {
+        'number_result': number_result,
+        'name_result': name_result,
+    }
+    return render(request, 'home.html', context)  # Corrected to pass the entire context
